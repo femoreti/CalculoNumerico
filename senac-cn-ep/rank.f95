c6f5d035b078804dbb5b884601fa2befafc267c3
Felipe Moreti <felipe_moreti7@hotmail.com>
2018-06-09 17:30:10 -0300

    fix some stuff
    giving result

--- senac-cn-ep/rank.f95
+++ senac-cn-ep/rank.f95
@@ -1,72 +1,64 @@
-program rank
-implicit none
-
-	!dimensoes de entrada, sempre devem ser iguais
-	integer :: matrixSize
-	integer :: i , j , iterations
+PROGRAM rank
+IMPLICIT NONE
+	INTEGER :: matrixSize
+	INTEGER :: i , j , iterations
 	
-	!matriz A, matriz S  |  vetor dimensao_1
-	double precision, dimension(:,:), allocatable :: matrix_A, matrix_S,vetor_pesos_aux
-	double precision :: precisao = 0.00001
-	double precision :: valor_antigo = 1
+	!matriz A(n,n), matriz S (n,1), resultado(n,1)
+	DOUBLE PRECISION, DIMENSION(:,:), ALLOCATABLE :: A, S, matrixResult
+	DOUBLE PRECISION :: stopValue = 1E-5
+	DOUBLE PRECISION :: temporaryValue
 
-	!Recebe a matriz do python ou le de um arquivo
+	!Temporario, modificar quando vir do python
 	matrixSize = 4
 
-	!Aloca dinamicamente a matriz com as dimensoes da matriz recebida
-	allocate ( matrix_A(matrixSize,matrixSize) )
-	allocate ( matrix_S(matrixSize,1) )
-	allocate ( vetor_pesos_aux(matrixSize,1) )
+	!Define os tamanhos das matrizes
+	allocate (A(matrixSize, matrixSize))
+	allocate (S(matrixSize, 1))
+	allocate (matrixResult(matrixSize, 1))
 
-
-	matrix_A(1,1) = 0
-	matrix_A(1,2) = 0
-	matrix_A(1,3) = 1.0
-	matrix_A(1,4) = 1.0/2
-	matrix_A(2,1) = 1.0/3
-	matrix_A(2,2) = 0
-	matrix_A(2,3) = 0
-	matrix_A(2,4) = 0
-	matrix_A(3,1) = 1.0/3
-	matrix_A(3,2) = 1.0/2
-	matrix_A(3,3) = 0
-	matrix_A(3,4) = 1.0/2
-	matrix_A(4,1) = 1.0/3
-	matrix_A(4,2) = 1.0/2
-	matrix_A(4,3) = 0
-	matrix_A(4,4) = 0
-
-
-
-	! Multiplica os valores da matrix_A por (1-m)
-
-	do i=1,matrixSize
-		matrix_S(i,1) = (1.0/matrixSize) 
-		vetor_pesos_aux(i,1) = 1.0/matrixSize
-	end do
-
-
+	!Temporario, ler do python depois
+	A(1,1) = 0
+	A(1,2) = 0
+	A(1,3) = 1.0
+	A(1,4) = 1.0 / 2
+	A(2,1) = 1.0 / 3
+	A(2,2) = 0
+	A(2,3) = 0
+	A(2,4) = 0
+	A(3,1) = 1.0 / 3
+	A(3,2) = 1.0 / 2
+	A(3,3) = 0
+	A(3,4) = 1.0 / 2
+	A(4,1) = 1.0 / 3
+	A(4,2) = 1.0 / 2
+	A(4,3) = 0
+	A(4,4) = 0
 	
-
-
-	iterations = 0
-	! Multiplica a matrix_A por matrix_S até um threshold
-	do while (ABS(valor_antigo-vetor_pesos_aux(1,1)) > precisao )
-		valor_antigo = vetor_pesos_aux(1,1)
-		matrix_A = matmul(matrix_A,matrix_A)
-	    vetor_pesos_aux = matmul(matrix_A,matrix_S)
-		iterations = iterations + 1
+	!inicia a matriz S e a matriz de resultado com tamanho (n,1)
+	do i = 1, matrixSize
+		S(i,1) = (1.0 / matrixSize) 
+		matrixResult(i,1) = 1.0 / matrixSize
+	end do
+	
+	!iterations = 0
+	temporaryValue = 1
+	! Multiplica a matriz A pela matriz S ate a precisao ser atingida
+	do while (ABS(temporaryValue -  matrixResult(1,1)) > stopValue)
+		temporaryValue = matrixResult(1,1)
+		A = matmul(A,A)
+	    matrixResult = matmul(A,S)
+		!iterations = iterations + 1
 	end do
 
-
-	do i=1,matrixSize
-		print *,vetor_pesos_aux(i,1)
+	!Imprime os resultados dos pesos de cada nó do grafo
+	do i = 1, matrixSize
+		print *, matrixResult(i, 1)
 	end do
 
 	!print *, iterations
-
-
-	deallocate( matrix_A )
-	deallocate( matrix_S )
-	deallocate( vetor_pesos_aux)
-end program rank
+	
+	!remove da memoria as matrizes criadas
+	deallocate(A)
+	deallocate(S)
+	deallocate(matrixResult)
+END PROGRAM rank
